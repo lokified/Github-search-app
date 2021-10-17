8abd7cde00c65ef4808bc4ee3c9b537be638ae40
lokified <sheldon.obonyo@student.moringaschool.com>
2021-10-17 23:06:31 +0300

    add a function that get profiles from API

--- src/app/users/user-name/user/user.component.ts
+++ src/app/users/user-name/user/user.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/user-class/user';
-import { environment } from 'src/environments/environment';
+
 
 
 @Component({
@@ -11,26 +11,25 @@ import { environment } from 'src/environments/environment';
 })
 export class UserComponent implements OnInit {
 
-  users : any [] = [];
-  nameInput :string = '';
-  repoNumber! : number;
-  isLoading = true;
+  profile: any;
+  repos: any;
+  userName: any = '';
+ 
 
   constructor(private userService : UserService) {
 
    }
 
+    findProfile() {
+     this.userService.updateProfile(this.userName);
+  
+     this.userService.getUserInfo().then( profile =>{
+       console.log(profile)
+     this.profile = profile; 
+    });
+
+  }
   ngOnInit(): void {
-    this.getPublicRepositories();
-  }
-
-  getPublicRepositories () {
-    this.userService.getUsers(this.nameInput).then( (response: any[]) => {
-      this.users = response;
-      this.repoNumber =response.length;
-    }).catch( (err)=> {
-      console.log(err);
-    })
-  }
-
+    this.findProfile();
+   }
 }
